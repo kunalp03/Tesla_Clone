c96e81cc319c13cd6b423c21129c566d6b82a7b8
Kunal Patil <kp327313@gmail.com>
2022-07-17 11:37:17 +0530

    third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components"
-
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
     return (
         <Container>
             <a>
@@ -17,7 +19,21 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
+                <CustomMenu onClick={()=>setBurgerStatus(true)}/>
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={()=>setBurgerStatus(false)} />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -25,14 +41,17 @@ function Header() {
 export default Header
 
 const Container = styled.div`
+    z-index: 1;
     min-height: 60px;
     position: fixed;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 0 20px;
     top: 0;
     left: 0;
     right: 0;
+    
 `
 
 const Menu = styled.div`
@@ -46,12 +65,54 @@ const Menu = styled.div`
         padding: 0 10px;
         flex-wrap: nowrap;
     }
+
+    @media(max-width: 768px){
+        display: none;
+    }
 `
 
 const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
     a{
         font-weight: 600;
         text-transform: uppercase;
         margin-right: 10px;
     }
+`
+
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+        a{
+            font-weight: 600;
+        }
+    }
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const CloseWrapper = styled.div `
+    display: flex;
+    justify-content: flex-end;
 `
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
-                    {rightBtnText &&
-                        <RightButton>
-                            {rightBtnText}
-                        </RightButton>
-                    }
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {rightBtnText &&
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
 
                 <DownArrow src="/images/down-arrow.svg" />
             </Buttons>
@@ -45,6 +50,7 @@ const Wrap = styled.div`
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
+    z-index: -1;
 `
 
 const ButtonGroup = styled.div`
